0630b4b05fdf162c68db199239e5702a61c9f037
Luka Radovanovic <lukadlm97@gmail.com>
2020-01-24 19:40:30 +0100

    Created DBContext class and models

---
+++ StudentService/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace StudentServiceModel.Model
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+        public string LastName { get; set; }
+        public string FirstMidName { get; set; }
+        public DateTime EntollmentDate { get; set; }
+
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
---
+++ StudentServiceModel/StudentServiceContext.cs
@@ -0,0 +1,20 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using StudentServiceModel.Model;
+using System;
+
+
+
+namespace StudentServiceModel
+{
+    public class StudentSeriviceDbContext :DbContext
+    {
+        public StudentSeriviceDbContext(DbContextOptions<StudentSeriviceDbContext> options) : base(options) 
+        {
+
+        }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+    }
+}
